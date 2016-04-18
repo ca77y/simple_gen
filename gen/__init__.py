966a657b5f837adff40ccc9d35220c9e8edec401
ca77y <ca77y@live.com>
2016-04-18 06:44:58 +0200

    generations

--- gen/__init__.py
+++ gen/__init__.py
@@ -1,12 +1,16 @@
 from .population import init_population, multiply, mutate
-from .gosia import kill_ugly
+from .gosia import kill_ugly, best_tofik
 from pprint import pprint
+
+MAX_GENERATIONS = 100
 
 
 def find_match():
     pop = init_population()
-    pop = kill_ugly(pop)
-    kids = multiply(pop)
-    kids = mutate(kids)
-    pop.extend(kids)
+    for gen in range(0, MAX_GENERATIONS):
+        pop = kill_ugly(pop)
+        kids = multiply(pop)
+        kids = mutate(kids)
+        pop.extend(kids)
+    pprint(best_tofik)
     pprint(pop)
